7a1c009c555cc50a18a7b2b7f503eae80bf4bd4d
toksiska <150230467+toksiska@users.noreply.github.com>
2024-08-08 15:44:55 +0700

    Update main.js

--- main.js
+++ main.js
@@ -49,3 +49,13 @@ ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+/*================== typed js ==================*/
+const typed = new Typed('.multiple-text', {
+    strings: ['High School Students', 'Software Engineering',],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
